e9a25a27fa14510d51321ef64882f23d53dc7cf8
Leonardo-Corazzini <leocora10@gmail.com>
2025-02-27 12:30:13 +0100

    snack 3 e 4

--- script.js
+++ script.js
@@ -25,3 +25,43 @@
 // P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console? salad due volte
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 2
+// 🏆 Code Question 3
+// const hamburger = {
+// 	name: "Cheese Burger",
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const secondBurger = structuredClone(hamburger);
+// const thirdBurger = structuredClone(hamburger);
+
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 3
+// 🏆 Code Question 4
+// const chef = {
+//     name: "Chef Hyur",
+//     age: 29,
+//     makeBurger: (num = 1) => {
+//         console.log(`Ecco ${num} hamburger per te!`);
+//     },
+// }
+
+// const restaurant = {
+//     name: "Hyur's Burgers",
+//     address: {
+//         street: 'Main Street',
+//         number: 123,
+//     },
+//     openingDate: new Date(2025, 3, 11),
+//     isOpen: false,
+// };
+
+// Qual è il metodo migliore per clonare l’oggetto chef, e perché? tramite lo spread operatore perche  e un oggetto con una struttura semplice ed ha all'interno una proprieta con una funzione e tyramite lo spred si possono copiare anche quelle.
+// Qual è il metodo migliore per clonare l’oggetto restaurant, e perché? tramite structuredClone() perche e un oggetto con una struttura complessa.
